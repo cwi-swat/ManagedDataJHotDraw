e86d3cdb25bf56b0f1dd15737fa21f9d1bef354f
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2016-04-20 23:18:19 +0200

    extend MDRect

--- ManagedDataJHotDraw/src/ccconcerns/managed_data/schemas/geometry/MDRectangle.java
+++ ManagedDataJHotDraw/src/ccconcerns/managed_data/schemas/geometry/MDRectangle.java
@@ -8,6 +8,36 @@ public interface MDRectangle extends M {
     Integer width(Integer... width);
     Integer height(Integer... height);
 
+    default void add(MDRectangle r) {
+        long tx2 = width();
+        long ty2 = height();
+        if ((tx2 | ty2) < 0) {
+            reshape(r.x(), r.y(), r.width(), r.height());
+        }
+        long rx2 = r.width();
+        long ry2 = r.height();
+        if ((rx2 | ry2) < 0) {
+            return;
+        }
+        int tx1 = this.x();
+        int ty1 = this.y();
+        tx2 += tx1;
+        ty2 += ty1;
+        int rx1 = r.x();
+        int ry1 = r.y();
+        rx2 += rx1;
+        ry2 += ry1;
+        if (tx1 > rx1) tx1 = rx1;
+        if (ty1 > ry1) ty1 = ry1;
+        if (tx2 < rx2) tx2 = rx2;
+        if (ty2 < ry2) ty2 = ry2;
+        tx2 -= tx1;
+        ty2 -= ty1;
+
+        if (tx2 > Integer.MAX_VALUE) tx2 = Integer.MAX_VALUE;
+        if (ty2 > Integer.MAX_VALUE) ty2 = Integer.MAX_VALUE;
+        reshape(tx1, ty1, (int) tx2, (int) ty2);
+    }
 
     default boolean contains(int x, int y) {
         return inside(x, y);
