3a8b5d4ba627c369f1ca2a8d2dba51236397acf0
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2016-05-06 11:24:51 +0200

    Remove local invocation of default methods from concrete MObjects

--- src/ccconcerns/managed_data/data_managers/SubjectRole/SubjectRoleMObject.java
+++ src/ccconcerns/managed_data/data_managers/SubjectRole/SubjectRoleMObject.java
@@ -3,7 +3,6 @@ package ccconcerns.managed_data.data_managers.SubjectRole;
 import nl.cwi.managed_data_4j.language.managed_object.MObject;
 import nl.cwi.managed_data_4j.language.schema.models.definition.Klass;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,18 +27,5 @@ public class SubjectRoleMObject extends MObject implements SubjectRole {
     @Override
     public void removeListener(Object listener) {
         listeners.remove(listener);
-    }
-
-    @Override
-    protected Object _invokeDefaultMethod(Object proxy, Method method, Object[] args) throws Throwable {
-        for (Method objMethod : this.getClass().getMethods()) {
-            if (method.getName().equals(objMethod.getName()) &&
-                method.getReturnType().equals(objMethod.getReturnType()))
-            {
-                System.out.println("Going to call: " + method.getName());
-                objMethod.invoke(this, args);
-            }
-        }
-        return super._invokeDefaultMethod(proxy, method, args);
     }
 }
--- src/ccconcerns/undo/UndoableMObject.java
+++ src/ccconcerns/undo/UndoableMObject.java
@@ -4,8 +4,6 @@ import CH.ifa.draw.util.Undoable;
 import nl.cwi.managed_data_4j.language.managed_object.MObject;
 import nl.cwi.managed_data_4j.language.schema.models.definition.Klass;
 
-import java.lang.reflect.Method;
-
 public abstract class UndoableMObject extends MObject implements UndoableRole {
 
     private Undoable myUndoableActivity;
@@ -22,18 +20,5 @@ public abstract class UndoableMObject extends MObject implements UndoableRole {
     @Override
     public void setUndoActivity(Undoable newUndoableActivity) {
         myUndoableActivity = newUndoableActivity;
-    }
-
-    @Override
-    protected Object _invokeDefaultMethod(Object proxy, Method method, Object[] args) throws Throwable {
-        for (Method objMethod : this.getClass().getMethods()) {
-            if (method.getName().equals(objMethod.getName()) &&
-                    method.getReturnType().equals(objMethod.getReturnType()))
-            {
-                System.out.println("Going to call: " + method.getName());
-                objMethod.invoke(this, args);
-            }
-        }
-        return super._invokeDefaultMethod(proxy, method, args);
     }
 }
